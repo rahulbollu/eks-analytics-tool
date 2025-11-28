441b423e2e2e5e5da01556bec70d087a664972c7
Rahul bollu <bvrahul3141@gmail.com>
2025-11-28 15:57:04 +0530

    updated domain name

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,13 +1,13 @@
 locals {
   name        = "eks-cluster"
   domain      = "ahmedumami.click"
-  region      = "eu-west-2"
-  hosted_zone = "arn:aws:route53:::hostedzone/Z103935430WUS287YMWJ6"
-  zone_id     = "Z103935430WUS287YMWJ6"
+  region      = "us-east-1"
+  hosted_zone = ""
+  zone_id     = ""
 
 
   tags = {
     project = "EKS PROJECT"
-    owner   = "Ahmed"
+    owner   = "Rahul"
   }
 }
